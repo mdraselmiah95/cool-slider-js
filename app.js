cb8c2ca4680e5e39ee1e1cab8ef57fde716fc8a2
rasel <russelroy15@gmail.com>
2021-09-14 18:43:58 +0600

    WIP on cool slider

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const showImages = (images) => {
   // show gallery title
   galleryHeader.style.display = "flex";
   images.forEach((image) => {
-    console.log(image);
+    // console.log(image);
     let div = document.createElement("div");
     div.className = "col-lg-3 col-md-4 col-xs-6 img-item mb-2";
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
